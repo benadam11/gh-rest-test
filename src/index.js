b6d9a0665c66e6e58e812824348ad490fd0c9341
Benjamin Adam <benjamin.mark.adam@gmail.com>
2022-08-15 15:25:39 -0700

    Update directory names

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import "./state-practice-1/theme.css";
-import "./state-practice-1/styles.css";
-import App from "./state-solution-1/App";
+import "./react/practice-state/state-practice-1/theme.css";
+import "./react/practice-state/state-practice-1/styles.css";
+import App from "./react/solution-state/state-solution-1/App";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
